0507c852ab68c6fe7482e3699ff591900370554c
Javier Romero <jromeroschmidt@gmail.com>
2021-08-06 09:51:00 -0400

    Copy from boilerplate

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import path from 'path'
+
+import { environment } from './src/config'
+import { databaseLoader, expressLoader } from './src/loaders'
+
+const startServer = async () => {
+    await databaseLoader()
+
+    const app = await expressLoader()
+
+    if (environment.env === 'production') {
+        console.info('The server is in production mode')
+        app.use(express.static(path.resolve(__dirname, '../client/build')))
+
+        app.get('/*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+        })
+    }
+
+    app.listen(environment.port, (err) => {
+        if (err) {
+            console.error('Express startup error: ', err)
+            throw err
+        }
+
+        console.info(`Express server started in port: ${environment.port}`)
+    })
+}
+
+startServer()
