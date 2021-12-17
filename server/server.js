97398e8137fb18e499d20891b24ec01bf35614df
Senuba <sebastian.nunez.b@hotmail.com>
2021-12-17 10:07:34 -0300

    package .json se saco el buildeo

--- server/server.js
+++ server/server.js
@@ -1,57 +1,46 @@
-import express from 'express'
-import path from 'path'
-import { ApiIcv } from './src/api-icv';
+import express from "express";
+import path from "path";
+import { ApiIcv } from "./src/api-icv";
 
-import { environment } from './src/config'
-import { databaseLoader, expressLoader, apiIcvLoader } from './src/loaders'
-import { AccessControlServices } from './src/services'
+import { environment } from "./src/config";
+import { databaseLoader, expressLoader, apiIcvLoader } from "./src/loaders";
+import { AccessControlServices } from "./src/services";
 
 const startServer = async () => {
-
-
     /* Conexión a MongoDB  */
 
-    await databaseLoader()
-    
+    await databaseLoader();
+
     /* Carga de recursos servidor */
     const app = await expressLoader();
 
-    
     /* Carga de recursos desde API ICV */
     await apiIcvLoader();
 
-
     /* Conexión a control de acceso a servidor */
     await AccessControlServices.initAccessControl();
-    
+
     /* Inicio de Servidor */
-    if(app) {
-        console.log('Ok APP')
+    if (app) {
+        console.log("Ok APP");
     }
 
-    if (environment.env === 'production') {
-        /* Solo se ejecuta en producción */
-        console.info('The server is in production mode')
-        app.use(express.static(path.resolve(__dirname, '../client/build')))
+    /* Solo se ejecuta en producción */
+    console.info("The server is in production mode");
+    app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-        app.get('/*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-        })
-    }else{
-        console.info('The server is in development mode')
-    }
+    app.get("/*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    });
 
     app.listen(environment.port, (err) => {
         if (err) {
-            console.error('Express startup error: ', err)
-            throw err
+            console.error("Express startup error: ", err);
+            throw err;
         }
 
-        console.info(`Express server started in port: ${environment.port}`)
-    })
+        console.info(`Express server started in port: ${environment.port}`);
+    });
+};
 
-
-    
-}
-
-startServer()
+startServer();
