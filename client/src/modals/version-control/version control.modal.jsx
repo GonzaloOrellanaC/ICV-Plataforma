7a422a9d3f0c1cc434f4dd7a41337d877c84f985
GonzaloOrellanaC <gonzalo.orellana@kahuel.com>
2021-12-30 18:37:22 -0300

    Beta 2.9 Control de Versiones en Modal actualizado

--- client/src/modals/version-control/version control.modal.jsx
+++ client/src/modals/version-control/version control.modal.jsx
@@ -16,33 +16,42 @@ const VersionControlModal = ({open, closeModal}) => {
                     <h2>Versión {environment.version}</h2>
                 </div>
                 <div style={{width: '100%', height: '25vh', overflowY: 'auto'}}>
-                        <p>
-                            Se ha actualizado la aplicación de servicios de Mantenimiento e Inspección.
-                        </p>
-                        <p>
-                            v.2.5
-                                <li>
-                                    Se actualiza para responsividad smartphone, excepto control de usuarios usando admin o Ejecutivo SAP y reportes.
-                                </li>
-                                <li>
-                                    Se corrige error de redacción de correo electrónico. 
-                                </li>
-                        </p>
-                        <p>
-                            v.2.2
-                                <li>
-                                    Los usuarios pueden navegar sin conexión, de acuerdo a sus privilegios. 
-                                </li>
-                                <li>
-                                    Los usuarios que intenten acceder a una URL de la aplicación a la que no cuente con permisos, aparecerá un mensaje indicando que no tiene acceso. 
-                                </li>
-                                <li>
-                                    Se ejecuta lógica offline para máquinas 3D. 
-                                </li>
-                                <li>
-                                    Los administradores pueden asignar obra al crear o editar usuarios. 
-                                </li>
-                        </p>
+                    <p>
+                        Se ha actualizado la aplicación de servicios de Mantenimiento e Inspección.
+                    </p>
+                    <p>
+                        <strong>v.2.9</strong>
+                        <li>
+                            Se habilita restablecimiento de contraseña.
+                        </li>
+                        <li>
+                            Se agregan imágenes en formatos SVG, JPG y PNG con mayor resolución.
+                        </li>
+                    </p>
+                    <p>
+                        <strong>v.2.5</strong>
+                        <li>
+                            Se actualiza para responsividad smartphone, excepto control de usuarios usando admin o Ejecutivo SAP y reportes.
+                        </li>
+                        <li>
+                            Se corrige error de redacción de correo electrónico. 
+                        </li>
+                    </p>
+                    <p>
+                        <strong>v.2.2</strong>
+                        <li>
+                            Los usuarios pueden navegar sin conexión, de acuerdo a sus privilegios. 
+                        </li>
+                        <li>
+                            Los usuarios que intenten acceder a una URL de la aplicación a la que no cuente con permisos, aparecerá un mensaje indicando que no tiene acceso. 
+                        </li>
+                        <li>
+                            Se ejecuta lógica offline para máquinas 3D. 
+                        </li>
+                        <li>
+                            Los administradores pueden asignar obra al crear o editar usuarios. 
+                        </li>
+                    </p>
                 </div>
                 <Fab onClick={() => closeModal()} style={{position: 'absolute', right: 10, top: 10, boxShadow: 'none', backgroundColor: 'transparent'}}>
                     <Close style={{color: '#ccc'}} />
