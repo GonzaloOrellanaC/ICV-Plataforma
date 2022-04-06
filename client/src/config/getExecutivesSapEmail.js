edc282dd1761ad2415739ffc6ca85ac2b84dd8e9
GonzaloOrellanaC <gonzalo.orellana@kahuel.com>
2022-04-06 16:28:57 -0400

    version 1.0 rev5

--- client/src/config/getExecutivesSapEmail.js
+++ client/src/config/getExecutivesSapEmail.js
@@ -7,26 +7,28 @@ export default async (level) => {
         let data = await usersRoutes.findByRole('sapExecutive');
         let data1 = await usersRoutes.findByRole('shiftManager');
         let data2 = await usersRoutes.findByRole('chiefMachinery');
-        let admins = new Array();
-        let sapExecutives = new Array();
-        admins = admin.data;
-        sapExecutives = data.data;
+        console.log(admin.data, data.data, data1.data, data2.data)
+        let admins = admin.data;
+        let sapExecutives = data.data;
         let chiefMachineries = data1.data;
         let shiftManagers = data2.data;
-        let emails = new Array();
-        emails = []
+        let emails = []
         admins.forEach((admin, index) => {
             emails.push(admin.email);
+            console.log(emails)
             if(index == (admins.length - 1)) {
                 sapExecutives.forEach((user, i) => {
                     emails.push(user.email);
+                    console.log(emails)
                     if(i == (sapExecutives.length - 1)) {
                         if(level == 1) {
                             chiefMachineries.forEach((u, n) => {
                                 emails.push(u.email);
+                                console.log(emails)
                                 if(n == (chiefMachineries.length - 1)) {
                                     shiftManagers.forEach((s, x) => {
                                         emails.push(s.email)
+                                        console.log(emails)
                                     });
                                     if(n == (shiftManagers.length - 1)) {
                                         resolve(emails.toString())
@@ -36,6 +38,7 @@ export default async (level) => {
                         }else if(level == 2) {
                             chiefMachineries.forEach((u, n) => {
                                 emails.push(u.email);
+                                console.log(emails)
                                 if(n == (chiefMachineries.length - 1)) {
                                     resolve(emails.toString())
                                 }
