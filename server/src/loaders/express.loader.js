b69df1c216febc13b983f7f470af593b9a7c2177
GonzaloOrellanaC <gonzalo.orellana@kahuel.com>
2022-08-16 18:39:56 -0400

    ICV version 1.9 Beta 3

--- server/src/loaders/express.loader.js
+++ server/src/loaders/express.loader.js
@@ -20,8 +20,8 @@ export default async () => {
 
     console.log('Iniciando node JS')
 
-    app.use(express.json({ limit: '50mb', extended: true }))
-    app.use(express.urlencoded({ limit: '50mb', extended: true }))
+    app.use(express.json({ limit: '200mb', extended: true }))
+    app.use(express.urlencoded({ limit: '200mb', extended: true }))
     app.use(cookieParser())
     app.use(fileUpload());
     /* Configure Passport */
