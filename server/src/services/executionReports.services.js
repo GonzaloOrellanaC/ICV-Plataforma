05150dbe39c2e240bc9a573c3e2cc7d68299f4d4
GonzaloOrellanaC <gonzalo.orellana@kahuel.com>
2022-08-18 13:08:33 -0400

    ICV version 1.9 Beta 23

--- server/src/services/executionReports.services.js
+++ server/src/services/executionReports.services.js
@@ -47,6 +47,9 @@ const createNewExecutionReport = async (executionReport) => {
     })
 }
 
+let numberVerification1 = 0
+let numberVerification2 = 0
+
 const saveExecutionReport = async (req, res) => {
     const { body } = req
     console.log(body);
@@ -54,28 +57,13 @@ const saveExecutionReport = async (req, res) => {
     const response = await Reports.findById(executionReportData.reportId)
     const report = response.toJSON()
     console.log(report.idIndex)
-    if (report) {
-        res.json({data: 'ok'})
-    } else {
-        res.json({data: 'error'})
-    }
-    /* let imagesList = [] */
     Object.keys(executionReportData.group).forEach(async (key, index) => {
-        console.log(index, Object.keys(executionReportData.group).length)
-        executionReportData.group[key].forEach(item => {
+        executionReportData.group[key].forEach((item) => {
             if (item.messages) {
                 item.messages.forEach(async (mensaje, i) => {
                     if (mensaje.urlBase64) {
-                        /* const imageData = {
-                            urlBase64: mensaje.urlBase64,
-                            reportIdIndex: report.idIndex,
-                            key: key,
-                            id: mensaje.id
-                        }
-                        imagesList.push(imageData)
-                        mensaje.urlImageMessage = imageData.data.url
-                        mensaje.urlBase64 = '' */
                         if (mensaje.urlBase64.length > 0) {
+                            numberVerification1 = numberVerification1 + 1
                             const imageData = await AzureServices.uploadImageFromReport(
                                 mensaje.urlBase64,
                                 report.idIndex,
@@ -83,92 +71,66 @@ const saveExecutionReport = async (req, res) => {
                                 mensaje.id
                             )
                             console.log(imageData)
+                            if (imageData) {
+                                numberVerification2 = numberVerification2 + 1
+                            }
                             mensaje.urlImageMessage = imageData.data.url
                             mensaje.urlBase64 = ''
                         }
                     }
-                    /* await saveExecutionReportInternal(executionReportData) */
                     if (i === (item.messages.length - 1)) {
                     }
                 })
             }
         })
         if (index == (Object.keys(executionReportData.group).length - 1)) {
-            /* if (imagesList.length > 0) {
-
-            } */
-            if (executionReportData.astList.length > 0) {
+            if (executionReportData.astList && (executionReportData.astList.length > 0)) {
                 executionReportData.astList.forEach(async (ast, i) => {
+                    numberVerification1 = numberVerification1 + 1
                     const imageAstData = await AzureServices.uploadImageAstFromReport(
                         ast.image,
                         report.idIndex,
                         ast.id
                     )
                     console.log(imageAstData)
+                    if (imageAstData) {
+                        numberVerification2 = numberVerification2 + 1
+                    }
                     ast.image = ''
                     ast.imageUrl = imageAstData.data.url
-                    /* await saveExecutionReportInternal(executionReportData) */
                     if (i == (executionReportData.astList.length - 1)) {
-                        await saveExecutionReportInternal(executionReportData)
+                        timeout(executionReportData, res)
                     }
                 })
             } else {
-                await saveExecutionReportInternal(executionReportData)
+                timeout(executionReportData, res)
             }
         }
     })
 }
 
-const saveDataOne = (executionReportData, report) => {
-    return new Promise(resolve => {
-        executionReportData.group[key].forEach(item => {
-            if (item.messages) {
-                item.messages.forEach(async (mensaje, i) => {
-                    if (mensaje.urlBase64) {
-                        /* const imageData = {
-                            urlBase64: mensaje.urlBase64,
-                            reportIdIndex: report.idIndex,
-                            key: key,
-                            id: mensaje.id
-                        }
-                        imagesList.push(imageData)
-                        mensaje.urlImageMessage = imageData.data.url
-                        mensaje.urlBase64 = '' */
-                        if (mensaje.urlBase64.length > 0) {
-                            const imageData = await AzureServices.uploadImageFromReport(
-                                mensaje.urlBase64,
-                                report.idIndex,
-                                key,
-                                mensaje.id
-                            )
-                            console.log(imageData)
-                            mensaje.urlImageMessage = imageData.data.url
-                            mensaje.urlBase64 = ''
-                        }
-                    }
-                    /* await saveExecutionReportInternal(executionReportData) */
-                    if (i === (item.messages.length - 1)) {
-                    }
-                })
+const timeout = (executionReportData, res) => {
+    console.log('Verificaciones: ', numberVerification1, numberVerification2)
+    setTimeout(async() => {
+        if (numberVerification1 == numberVerification2) {
+            let response = await saveExecutionReportInternal(executionReportData)
+            console.log(response)
+            if (response) {
+                res.json({data: 'ok'})
+            } else {
+                const newDoc = await createNewExecutionReport(executionReportData);
+                console.log(newDoc)
+                if(newDoc) {
+                    res.json(newDoc);
+                } else {
+                    res.json({data: 'error'})
+                }
             }
-        })
-    })
+        } else {
+            timeout(executionReportData, res)
+        }
+    }, 1000);
 }
-
-/* const saveByOne = (n, imagesList) => {
-    if (n == (imagesList.length - 1)) {
-
-    } else {
-        imagesList[n]
-        const imageData = await AzureServices.uploadImageFromReport(
-            imagesList[n].urlBase64,
-            imagesList[n].reportIdIndex,
-            imagesList[n].key,
-            imagesList[n].id
-        )
-    }
-
-} */
 
 const saveExecutionReportInternal = async (executionReportData) => {
     try{
